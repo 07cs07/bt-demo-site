ca64a2788b8fb1d89d15fff1497384d4412f5112
Dennis Won <jhwon0820@gmail.com>
2023-10-26 05:11:29 +0000

    integrate pm2 and ngrok for peristance

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ var express = require('express');
 var server = express();
 
 server.get('/.well-known/apple-app-site-association', function(request, response) {
-  response.sendFile(__dirname +  '/apple-app-site-association');
+  console.log(__dirname);
+  response.sendFile('/home/ec2-user/apple-app-site-association/apple-app-site-association');
+});
+
+server.get('/', function(request, response) {
+  response.send("Hello World");
 });
 
 server.listen(80);
